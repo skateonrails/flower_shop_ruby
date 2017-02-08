bf086f16ab0907f721ea629eb265b44fdb58937d
Samuel Prado <darkms@gmail.com>
2017-02-07 22:02:06 -0200

    Improving OrderInput class to parse input lines

--- app/order_input.rb
+++ app/order_input.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
-# OrderInput receives the input to create an order
+# OrderInput receives the input for the order
+# and transform each line into a OrderInputLine
 class OrderInput
-  attr_reader :input
+  attr_reader :input, :lines
 
   def initialize(args)
+    @lines = []
     @input = args[:input]
+    parse_lines
+  end
+
+  private
+
+  attr_writer :lines
+
+  def parse_lines
+    input.each_line do |input_line|
+      begin
+        lines << OrderInputLine.new(value: input_line)
+      rescue InvalidInputLineException
+        # ignoring parsing errors by now
+        nil
+      end
+    end
   end
 end
--- spec/app/order_input_spec.rb
+++ spec/app/order_input_spec.rb
@@ -2,12 +2,23 @@
 require 'spec_helper'
 
 describe OrderInput do
-  let(:input) { '10 R15' }
+  let(:filepath) { File.join(File.dirname(__FILE__), %w(.. input_examples example1.md)) }
+  let(:input) { File.new(filepath) }
   let(:order_input) { OrderInput.new(input: input) }
 
   describe '#input' do
     it 'should return the input from an order' do
       expect(order_input.input).to eq(input)
+    end
+  end
+
+  describe '#lines' do
+    it 'should return an array of objects that respond to product_code and quantity' do
+      expect(order_input.lines).to be_kind_of(Array)
+      order_input.lines.each do |line|
+        expect(line).to respond_to(:product_code)
+        expect(line).to respond_to(:quantity)
+      end
     end
   end
 end
