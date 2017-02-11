0886213c5e06c549b8d3a87ab52cfabf9f149730
Samuel Prado <darkms@gmail.com>
2017-02-11 17:18:03 -0200

    Create dependencies file to use in spec

---
+++ dependencies.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require File.join(File.dirname(__FILE__), %w(app exceptions.rb))
+require File.join(File.dirname(__FILE__), %w(app order_input_line.rb))
+require File.join(File.dirname(__FILE__), %w(app order_input.rb))
+require File.join(File.dirname(__FILE__), %w(app customer_order.rb))
+require File.join(File.dirname(__FILE__), %w(app product.rb))
+require File.join(File.dirname(__FILE__), %w(app product_storage.rb))
+require File.join(File.dirname(__FILE__), %w(app bundle.rb))
+require File.join(File.dirname(__FILE__), %w(app order_item.rb))
