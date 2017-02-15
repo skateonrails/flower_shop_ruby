67697e545e0c07dee4383a1f8b35e1a6d73432f4
Samuel Prado <darkms@gmail.com>
2017-02-14 23:53:21 -0200

    Removing CustomerOrder from collections folder

--- dependencies.rb
+++ dependencies.rb
@@ -18,10 +18,10 @@ require File.join(models_path, 'customer_order_item.rb')
 require File.join(models_path, 'product.rb')
 require File.join(models_path, 'bundle.rb')
 require File.join(models_path, 'package.rb')
+require File.join(models_path, 'customer_order.rb')
 # requiring collections
 require File.join(collections_path, 'bundle_collection.rb')
 require File.join(collections_path, 'order_input.rb')
-require File.join(collections_path, 'customer_order.rb')
 # requiring factories
 require File.join(factories_path, 'bundle_collection_factory.rb')
 require File.join(factories_path, 'order_input_factory.rb')
